eeed6ab9678d6deee75889312c62fd32ebab9830
SwaroopDangal <swaroopdangal732@gmail.com>
2026-02-05 18:50:36 +0545

    add SQL scripts for courses, students, and enrollment tables with sample data..learnt about relationships

---
+++ relationship.sql
@@ -0,0 +1,49 @@
+CREATE TABLE customers(
+	cust_id SERIAL PRIMARY KEY,
+	cust_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE orders(
+	ord_id SERIAL PRIMARY KEY,
+	ord_date DATE NOT NULL,
+	price NUMERIC NOT NULL,
+	cust_id INTEGER NOT NULL,
+	FOREIGN KEY(cust_id) REFERENCES
+	customers(cust_id)
+);
+
+INSERT INTO customers (cust_name)
+VALUES
+('Raju'), ('Sham'), ('Paul'), ('Alex');
+
+INSERT INTO orders (ord_date, cust_id, price)
+VALUES
+('2024-01-01'
+, 1, 250.00),
+('2024-01-15'
+, 1, 300.00),
+('2024-02-01'
+, 2, 150.00),
+('2024-03-01'
+, 3, 450.00),
+('2024-04-04'
+, 2, 550.00);
+
+SELECT * FROM customers CROSS JOIN orders;
+
+SELECT * FROM customers c INNER JOIN orders o ON c.cust_id=o.cust_id;
+
+SELECT cust_name,COUNT(orders.ord_id) FROM customers INNER JOIN orders ON
+customers.cust_id=orders.cust_id GROUP BY cust_name;
+
+SELECT * FROM customers
+LEFT JOIN orders
+ON orders.cust_id=customers.cust_id;
+
+SELECT * FROM customers
+RIGHT JOIN orders
+ON orders.cust_id=customers.cust_id;
+
+
+
+
